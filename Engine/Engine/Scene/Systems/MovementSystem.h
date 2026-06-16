942ead3b37e132d9466c049fe3a6eaed445279bd
Divaker-26 <divakerupadhyay142@gmail.com>
2026-06-16 22:13:29 +0530

    rotation culling

--- Engine/Engine/Scene/Systems/MovementSystem.h
+++ Engine/Engine/Scene/Systems/MovementSystem.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <Engine/Scene/Component.h>
-
 #include <entt/entt.hpp>
+#include <cmath>
 
 namespace SIMPEngine
 {
@@ -18,6 +18,11 @@ namespace SIMPEngine
 
                 transform.position.x += velocity.vx * dt;
                 transform.position.y += velocity.vy * dt;
+
+                transform.rotation = std::fmod(transform.rotation, 360.0f);
+
+                if (transform.rotation < 0.0f)
+                    transform.rotation += 360.0f;
             }
         }
     };
